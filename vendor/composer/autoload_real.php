0adec5bb613f4a7ee171d647cc69ca3d5956f676
unknown <rabinmia7578@gmail.com>
2023-02-07 16:46:35 +0600

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,25 +31,18 @@ class ComposerAutoloaderInitec9eb9e6794726359916422c02240236
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitec9eb9e6794726359916422c02240236::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireec9eb9e6794726359916422c02240236($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitec9eb9e6794726359916422c02240236::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequireec9eb9e6794726359916422c02240236($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
