c432998bce04c39a940017694222e938215fa1ea
Masud Rana Tapu <91066147+masudranatapu@users.noreply.github.com>
2023-03-22 15:17:29 +0600

    done

--- routes/web.php
+++ routes/web.php
@@ -60,10 +60,9 @@ Route::get('clear', function () {
 
 // Installer Middleware
 Route::group(['middleware' => 'Installer'], function () {
+
     Route::get('/', [HomeController::class, 'index'])->name('home-locale');
-
     Auth::routes();
-
     // Pages
     Route::get('faq', [HomeController::class, 'faq'])->name('faq');
     Route::get('about-us', [HomeController::class, 'about'])->name('about');
@@ -73,62 +72,46 @@ Route::group(['middleware' => 'Installer'], function () {
     Route::get('terms-and-conditions', [HomeController::class, 'termsAndConditions'])->name('terms.and.conditions');
     Route::get('refund-policy', [HomeController::class, 'refundPolicy'])->name('refund.policy');
     Route::post('card/subscriber', [HomeController::class, 'postSubscriber'])->name('card.subscriber');
-
     Route::post('post-register', [AuthController::class, 'postRegister'])->name('post-register');
-
-
     // Web Tools
     // HTML
     Route::get('html-beautifier', [WebToolsController::class, 'htmlBeautifier'])->name('web.html.beautifier');
     Route::get('html-minifier', [WebToolsController::class, 'htmlMinifier'])->name('web.html.minifier');
-
     // CSS
     Route::get('css-beautifier', [WebToolsController::class, 'cssBeautifier'])->name('web.css.beautifier');
     Route::get('css-minifier', [WebToolsController::class, 'cssMinifier'])->name('web.css.minifier');
     Route::post('css-minifier', [WebToolsController::class, 'resultCssMinifier'])->name('web.result.css.minifier');
-
     // JS
     Route::get('js-beautifier', [WebToolsController::class, 'jsBeautifier'])->name('web.js.beautifier');
     Route::get('js-minifier', [WebToolsController::class, 'jsMinifier'])->name('web.js.minifier');
     Route::post('js-minifier', [WebToolsController::class, 'resultjsMinifier'])->name('web.result.js.minifier');
-
     // Random Password Generator
     Route::get('random-password-generator', [WebToolsController::class, 'randomPasswordGenerator'])->name('web.random.password.generator');
     Route::post('random-password-generator', [WebToolsController::class, 'resultRandomPasswordGenerator'])->name('web.result.random.password.generator');
-
     // Bcrypt Password Generator
     Route::get('bcrypt-password-generator', [WebToolsController::class, 'bcryptPasswordGenerator'])->name('web.bcrypt.password.generator');
     Route::post('bcrypt-password-generator', [WebToolsController::class, 'resultBcryptPasswordGenerator'])->name('web.result.bcrypt.password.generator');
-
     // MD5 Password Generator
     Route::get('md5-password-generator', [WebToolsController::class, 'md5PasswordGenerator'])->name('web.md5.password.generator');
     Route::post('md5-password-generator', [WebToolsController::class, 'resultMd5PasswordGenerator'])->name('web.result.md5.password.generator');
-
     // Random Word Generator
     Route::get('random-word-generator', [WebToolsController::class, 'randomWordGenerator'])->name('web.random.word.generator');
     Route::post('random-word-generator', [WebToolsController::class, 'resultRandomWordGenerator'])->name('web.result.random.word.generator');
-
     // Text counter
     Route::get('text-counter', [WebToolsController::class, 'textCounter'])->name('web.text.counter');
-
     // Lorem Generator
     Route::get('lorem-generator', [WebToolsController::class, 'loremGenerator'])->name('web.lorem.generator');
-
     // Emojies
     Route::get('emojies', [WebToolsController::class, 'emojies'])->name('web.emojies');
-
     // DNS Lookup
     Route::get('dns-lookup', [WebToolsController::class, 'dnsLookup'])->name('web.dns.lookup');
     Route::post('dns-lookup', [WebToolsController::class, 'resultDnsLookup'])->name('web.result.dns.lookup');
-
     // IP Lookup
     Route::get('ip-lookup', [WebToolsController::class, 'ipLookup'])->name('web.ip.lookup');
     Route::post('ip-lookup', [WebToolsController::class, 'resultIpLookup'])->name('web.result.ip.lookup');
-
     // Whois Lookup
     Route::get('whois-lookup', [WebToolsController::class, 'whoisLookup'])->name('web.whois.lookup');
     Route::post('whois-lookup', [WebToolsController::class, 'resultWhoisLookup'])->name('web.result.whois.lookup');
-
     Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin'], 'where' => ['locale' => '[a-zA-Z]{2}']], function () {
         // Dashboard
         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
@@ -174,7 +157,6 @@ Route::group(['middleware' => 'Installer'], function () {
         Route::get('pages', [SettingsController::class, 'pages'])->name('pages');
         Route::get('page/{id}', [SettingsController::class, 'editPage'])->name('edit.page');
         Route::post('save-page/{id}', [SettingsController::class, 'savePage'])->name('save.page');
-
         // Settings
         Route::get('settings', [SettingsController::class, 'settings'])->name('settings');
         Route::post('change-general-settings', [SettingsController::class, "changeGeneralSettings"])->name('change.general.settings');
@@ -182,7 +164,7 @@ Route::group(['middleware' => 'Installer'], function () {
         Route::post('change-payments-settings', [SettingsController::class, "changePaymentsSettings"])->name('change.payments.settings');
         Route::post('change-google-settings', [SettingsController::class, "changeGoogleSettings"])->name('change.google.settings');
         Route::post('change-email-settings', [SettingsController::class, "changeEmailSettings"])->name('change.email.settings');
-
+        // tax setting
         Route::get('tax-setting', [SettingsController::class, 'taxSetting'])->name('tax.setting');
         Route::post('update-tex-setting', [SettingsController::class, 'updateTaxSetting'])->name('update.tax.setting');
         Route::post('update-email-setting', [SettingsController::class, 'updateEmailSetting'])->name('update.email.setting');
@@ -196,14 +178,10 @@ Route::group(['middleware' => 'Installer'], function () {
         Route::post('check-update', [UpdateController::class, 'checkUpdate'])->name('check.update');
         Route::post('update-code', [UpdateController::class, 'updateCode'])->name('update.code');
     });
-
+    // user
     Route::group(['as' => 'user.', 'prefix' => 'user', 'namespace' => 'User', 'middleware' => ['auth', 'user'], 'where' => ['locale' => '[a-zA-Z]{2}']], function () {
-
         Route::get('plans', [CardController::class, 'plans'])->name('plans')->withoutMiddleware(['userPlan']);
-
-
         Route::middleware(['userPlan'])->group(function () {
-
             Route::get('dashboard', [userDashboard::class, 'index'])->name('dashboard');
             // Business Cards
             Route::get('cards', [CardController::class, 'cards'])->name('cards');
@@ -213,9 +191,6 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::get('card-subscriber/{card}', [CardController::class, 'subscriber'])->name('card.subscriber');
             Route::post('send-mail-subscriber', [CardController::class, 'sendMail'])->name('card.subscriber.send.mail');
             Route::get('subscriber-export/{card}', [CardController::class, 'subscriberExport'])->name('card.subscriber.export');
-
-
-
             // Business Store
             Route::get('stores', [CardController::class, 'getStores'])->name('stores');
             Route::get('whatsapp-stores', 'CardController@storeCards')->name('whatsapp-stores');
@@ -227,18 +202,15 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::get('delete-media/{id}', [MediaController::class, 'deleteMedia'])->name('media.delete');
             // Upload media images
             Route::post('multiple', [MediaController::class, 'multipleImages'])->name('multiple');
-
             Route::post('card/store', [CardController::class, 'postStore'])->name('card.store');
             Route::post('card/update/{id}', [CardController::class, 'postUpdate'])->name('card.update');
             Route::get('card/delete/{id}', [CardController::class, 'getDelete'])->name('card.delete');
-
             Route::post('card/upload_image', ['as' => 'card.upload_image', 'uses' => 'CardController@uploadImage']);
             Route::post('card/upload_logo', ['as' => 'card.upload_logo', 'uses' => 'CardController@uploadLogo']);
-
             Route::get('create-card', [CardController::class, 'CreateCard'])->name('create.card');
             Route::get('edit-card/{id}', [CardController::class, 'editCard'])->name('edit.card');
             Route::get('card/gallery-delete/{id}', [CardController::class, 'getDeleteGallery'])->name('card.gallery-delete');
-
+            // v card
             if (env('APP_TYPE') == 'VCARD' || env('APP_TYPE') == 'BOTH') {
                 // Create Business Card
                 // Route::get('create-card', [CardController::class, 'CreateCard'])->name('create.card');
@@ -270,7 +242,7 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::get('edit-business-hours/{id}', [EditCardController::class, 'businessHours'])->name('edit.business.hours');
             Route::post('update-business-hours/{id}', [EditCardController::class, 'updateBusinessHours'])->name('update.business.hours');
             Route::get('clear-business-hours/{id}', [EditCardController::class, 'clearBusinessHours'])->name('clear.business.hours');
-
+            // store
             if (env('APP_TYPE') == 'STORE' || env('APP_TYPE') == 'BOTH') {
                 // Create Store
                 Route::get('create-store', [StoreController::class, 'CreateStore'])->name('create.store');
@@ -278,23 +250,16 @@ Route::group(['middleware' => 'Installer'], function () {
                 Route::get('products/{id}', [StoreController::class, 'products'])->name('products');
                 Route::post('save-products/{id}', [StoreController::class, 'saveProducts'])->name('save.products');
             }
-
-
+            // category
             Route::prefix('category')->name('product.category.')->group(function () {
                 Route::get('/', [ProductCategoryController::class, 'index'])->name('index');
                 Route::post('/store', [ProductCategoryController::class, 'store'])->name('store');
                 Route::post('/update/{id}', [ProductCategoryController::class, 'update'])->name('destroy');
                 Route::get('/delete/{id}', [ProductCategoryController::class, 'destroy'])->name('destroy');
             });
-
-
-
-
-
             // Edit Store
             Route::get('edit-store/{id}', [StoreController::class, 'editStore'])->name('edit.store');
             Route::post('update-store/{id}', [StoreController::class, 'updateStore'])->name('update.store');
-
             // Product
             Route::get('products-list/{id}', [StoreController::class, 'storeProductsList'])->name('products.list');
             Route::get('add-products/{id}', [StoreController::class, 'addProducts'])->name('products.add');
@@ -302,16 +267,12 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::get('edit-products/{id}', [StoreController::class, 'editProducts'])->name('products.edit');
             Route::post('update-products/{id}', [StoreController::class, 'updateProducts'])->name('products.update');
             Route::get('delete-products/{id}', [StoreController::class, 'deleteProducts'])->name('products.delete');
-
-
             // Product variant
             Route::get('{card_id}/product/{product_id}/variants', [VariantController::class, 'index'])->name('product.variants');
             Route::post('product/{product_id}/variants/store', [VariantController::class, 'store'])->name('product.variants.store');
             Route::post('product/{variant}/variants/update', [VariantController::class, 'update'])->name('product.variants.update');
             Route::get('product/{variant}/variants/delete', [VariantController::class, 'delete'])->name('product.variants.delete');
-
             // Product variant Options
-
             Route::get('product/{product_id}/variants/{variant}', [VariantController::class, 'optionIndex'])->name('product.variants.option');
             Route::get('product/{product_id}/variants/create/{variant}', [VariantController::class, 'optioncreate'])->name('product.variants.option.create');
             Route::post('product/{product_id}/variants/store/{variant}', [VariantController::class, 'optionStore'])->name('product.variants.option.store');
@@ -319,13 +280,8 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::post('variants/{option}/update', [VariantController::class, 'optionUpdate'])->name('variants.option.update');
             Route::get('variants/{option}/delete', [VariantController::class, 'optionDelete'])->name('variants.option.delete');
 
-
-
             Route::get('{card_id}/orders', [OrderController::class, 'index'])->name('order.index');
             Route::get('{card_id}/orders/view/{id}', [OrderController::class, 'view'])->name('order.view');
-
-
-
             //Addtional Tootls -> QR Maker
             Route::get('tools/qr-maker', [AdditionalController::class, 'qrMaker'])->name('qr-maker');
             Route::get('tools/whois-lookup', [AdditionalController::class, 'whoisLookup'])->name('whois-lookup');
@@ -337,9 +293,8 @@ Route::group(['middleware' => 'Installer'], function () {
             Route::prefix('/setting')->name('setting.')->group(function () {
                 Route::get('/payment', [UserSettingsController::class, 'payment'])->name('payment');
                 Route::post('/paymentUpdate', [UserSettingsController::class, 'paymentUpdate'])->name('payment.update');
-
             });
-
+            // state
             Route::prefix('/state')->name('state.')->group(function () {
                 Route::get('/', [StateController::class, 'index'])->name('index');
                 Route::get('/create', [StateController::class, 'create'])->name('create');
@@ -349,7 +304,7 @@ Route::group(['middleware' => 'Installer'], function () {
                 Route::get('/{id}/status', [StateController::class, 'status'])->name('status');
                 Route::get('/{id}/delete', [StateController::class, 'delete'])->name('delete');
             });
-
+            // shipping_area
             Route::prefix('/shipping_area')->name('shipping_area.')->group(function () {
                 Route::get('/', [ShippingAreaController::class, 'index'])->name('index');
                 Route::get('/create', [ShippingAreaController::class, 'create'])->name('create');
@@ -359,8 +314,6 @@ Route::group(['middleware' => 'Installer'], function () {
                 Route::get('/{id}/delete', [ShippingAreaController::class, 'delete'])->name('delete');
             });
         });
-
-
         // Transactions
         Route::get('transactions', [userTransactions::class, 'indexTransactions'])->name('transactions');
         Route::get('view-invoice/{id}', [userTransactions::class, 'viewInvoice'])->name('view.invoice');
@@ -369,14 +322,10 @@ Route::group(['middleware' => 'Installer'], function () {
         Route::post('update-billing', [userTransactions::class, 'updateBilling'])->name('update.billing');
         // Checkout
         Route::get('checkout/{id}', [CardController::class, 'checkout'])->name('checkout');
-
         // Save Upgrade Plan
         Route::post('save-upgrade/{id}', [CardController::class, 'saveUpgrade'])->name('save.upgrade.plan');
-
         // View Preview Business Card
         Route::get('view-preview/{id}', [CardController::class, 'viewPreview'])->name('view.preview');
-
-
         // Account Setting
         Route::get('account', [userAccount::class, 'account'])->name('account');
         Route::get('edit-account', [userAccount::class, 'editAccount'])->name('edit.account');
@@ -384,37 +333,28 @@ Route::group(['middleware' => 'Installer'], function () {
         Route::get('change-password', [userAccount::class, 'changePassword'])->name('change.password');
         Route::post('update-password', [userAccount::class, 'updatePassword'])->name('update.password');
     });
-
     // Choose Payment Gateway
     Route::post('/prepare-payment/{planId}', [PaymentController::class, 'preparePaymentGateway'])->name('prepare.payment.gateway');
-
     // PayPal Payment Gateway
     Route::get('/payment-paypal/{planId}', [PaypalController::class, 'paywithpaypal'])->name('paywithpaypal');
     Route::get('/payment/status', [PaypalController::class, 'paypalPaymentStatus'])->name('paypalPaymentStatus');
-
     // RazorPay
     Route::get('payment-razorpay/{planId}', [RazorpayController::class, 'prepareRazorpay'])->name('paywithrazorpay');
     Route::get('razorpay-payment-status/{oid}/{paymentId}', [RazorpayController::class, 'razorpayPaymentStatus'])->name('razorpay.payment.status');
-
     // Stripe
     Route::get('/payment-stripe/{planId}', [StripeController::class, 'stripeCheckout'])->name('paywithstripe');
     Route::post('/stripe-payment-status/{paymentId}', [StripeController::class, 'stripePaymentStatus'])->name('stripe.payment.status');
     Route::get('/stripe-payment-cancel/{paymentId}', [StripeController::class, 'stripePaymentCancel'])->name('stripe.payment.cancel');
-
     // Offline
     Route::get('/payment-offline/{planId}', [OfflineController::class, 'offlineCheckout'])->name('paywithoffline');
     Route::post('/mark-offline-payment', [OfflineController::class, 'markOfflinePayment'])->name('mark.payment.payment');
-
     // Google Auth
     Route::get('/google-login', [LoginController::class, 'redirectToProvider'])->name('login.google');
     Route::get('/sign-in-with-google', [LoginController::class, 'handleProviderCallback']);
-
     // Profile
     // Route::get('{id}', [ProfileController::class, 'profile', 'ShareWidget'])->name('profile');
-
     Route::get('/download/{id}', [ProfileController::class, 'downloadVcard'])->name('download.vCard');
 });
-
 // product details
 Route::get('/details/{id}', [HomeController::class, 'productDetails'])->name('product.details');
 // cart
@@ -422,7 +362,6 @@ Route::get('{card_id}/cart/', [HomeController::class, 'cartPage'])->name('cart')
 Route::patch('/update-cart', [HomeController::class, 'update'])->name('update.cart');
 Route::delete('/remove-from-cart', [HomeController::class, 'remove'])->name('remove.from.cart');
 Route::post('/add-to-cart', [HomeController::class, 'addToCart'])->name('add.to.cart');
-
 // checkout
 Route::get('{card_id}/checkout', [HomeController::class, 'checkout'])->name('checkout');
 Route::post('{card_id}/checkout', [HomeController::class, 'checkoutStore'])->name('checkout.store');
@@ -431,8 +370,7 @@ Route::post('{card_id}/checkout/billing-address', [HomeController::class, 'check
 Route::get('{card_id}/checkout/payment', [HomeController::class, 'checkoutPayment'])->name('checkout.payment');
 Route::get('{card_id}/checkout/payment/stripe', [HomeController::class, 'checkoutPaymentSrtipe'])->name('checkout.payment.stripe');
 Route::post('{card_id}/checkout/payment/stripe/{paymentId}', [HomeController::class, 'checkoutPaymentSrtipeStore'])->name('checkout.payment.stripe.store');
-
-
+// getpreview
 Route::get('{cardurl}', [HomeController::class, 'getPreview'])->name('card.preview');
 Route::post('/place-email-order', [HomeController::class, 'placeEmailOrder'])->name('place.email.order');
 Route::get('download/{id}', [HomeController::class, 'downloadVcard'])->name('download.vCard');
