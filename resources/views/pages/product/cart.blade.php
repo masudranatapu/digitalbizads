b54298caff862421bf3c9c71f62d1183a147f83e
Mridul <mridul>
2023-03-29 16:44:08 +0600

    commit

--- resources/views/pages/product/cart.blade.php
+++ resources/views/pages/product/cart.blade.php
@@ -2,26 +2,28 @@
 @section('title', 'Product Cart')
 @section('content')
 
-
-
     <div class="cart_page mt-5 mb-5">
         <div class="container">
             <div class="heading mb-4">
                 <h3>Cart</h3>
             </div>
+            {{-- <div class="col-sm-12 col-lg-12 d-none d-md-block"> --}}
+
             <div id="view_cart_load">
                 <div class="card mb-4">
                     <div class="card-body">
                         <div class="table-responsive shopping-cart">
                             <div class="table-responsive">
-                                <table id="cart" class="table table-hover table-condensed">
+                                <table class="table table-hover table-condensed" id="cart">
                                     <thead>
                                         <tr>
-                                            <th style="width:10%">Photo</th>
+                                            <th class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"
+                                                style="width:10%">Photo</th>
                                             <th style="width:35%">Product</th>
-                                            <th style="width:15%">Price</th>
+                                            <th class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"
+                                                style="width:15%">Price</th>
                                             <th style="width:15%">Quantity</th>
-                                            <th style="width:20%" class="text-center">Subtotal</th>
+                                            <th class="text-center" style="width:20%">Subtotal</th>
                                             <th style="width:10%">Remove</th>
                                         </tr>
                                     </thead>
@@ -37,22 +39,26 @@
                                                     $line_total = $details['price'] * $details['quantity'];
                                                 @endphp
 
-                                                <tr data-id="{{ $id }}" class="align-middle">
-                                                    <td data-th="Photo">
+                                                <tr class="align-middle" data-id="{{ $id }}">
+                                                    <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"
+                                                        data-th="Photo">
 
-                                                        <img src="{{ getPhoto($details['product']['product_image']) }}"
-                                                            width="50" class="img-responsive" />
+                                                        <img class="img-responsive"
+                                                            src="{{ getPhoto($details['product']['product_image']) }}"
+                                                            width="50" />
 
                                                     </td>
                                                     <td data-th="Product">
                                                         <span>{{ $details['product']['product_name'] }}</span>
                                                     </td>
-                                                    <td data-th="Price">{{ getPrice($details['price']) }}</td>
+                                                    <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"
+                                                        data-th="Price">{{ getPrice($details['price']) }}</td>
                                                     <td data-th="Quantity">
-                                                        <input type="number" value="{{ $details['quantity'] }}"
-                                                            class="form-control quantity update-cart allownumericwithoutdecimal" />
+                                                        <input
+                                                            class="form-control quantity update-cart allownumericwithoutdecimalMobile"
+                                                            type="number" value="{{ $details['quantity'] }}" />
                                                     </td>
-                                                    <td data-th="Subtotal" class="text-center">
+                                                    <td class="text-center" data-th="Subtotal">
                                                         {{ getPrice($line_total) }}
                                                     </td>
                                                     <td class="actions" data-th="">
@@ -73,8 +79,11 @@
                                     @if (session('cart'))
                                         <tfoot>
                                             <tr>
-                                                <td colspan="4" class="text-end">
-                                                    <h5><strong> Total : </strong></h5>
+                                                <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"></td>
+                                                <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell"></td>
+                                                <td></td>
+                                                <td class="text-end">
+                                                    <h5><strong> Total </strong></h5>
                                                 </td>
                                                 <td class="text-center">{{ getPrice($total) }}</td>
                                                 <td></td>
@@ -87,40 +96,144 @@
                     </div>
                 </div>
             </div>
-
-
             <div class="card shopping_btn">
                 <div class="card-body">
 
                     <div class="d-flex justify-content-between">
                         <div class="button">
-                            <a href="{{ route('card.preview', $business_card_details->card_url) }}"
-                                class="btn btn-primary"><i class="fa fa-angle-left"></i> Back to
+                            <a class="btn btn-primary"
+                                href="{{ route('card.preview', $business_card_details->card_url) }}"><i
+                                    class="fa fa-angle-left"></i> Back to
                                 Shopping</a>
                         </div>
 
                         @if (session()->has('cart') && count(session()->get('cart')) > 0)
                             <div class="button">
-                                <a href="{{ route('checkout', ['cardUrl' => $business_card_details->card_url]) }}"
-                                    class="btn btn-success">Checkout</a>
+                                <a class="btn btn-success"
+                                    href="{{ route('checkout', ['cardUrl' => $business_card_details->card_url]) }}">Checkout</a>
                             </div>
                         @endif
                     </div>
                 </div>
             </div>
+            {{-- </div> --}}
+
+            {{-- <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-block d-sm-block d-md-none d-lg-none d-xl-none">
+                <div class="row">
+                    <table class="table table-hover table-condensed" id="cart">
+                        <thead>
+                            <tr>
+                                <th style="width:10%">Photo</th>
+                                <th style="width:35%">Product</th>
+                                <th style="width:15%">Price</th>
+                                <th style="width:15%">Quantity</th>
+                                <th class="text-center" style="width:20%">Subtotal</th>
+                                <th style="width:10%">Remove</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @php
+                                $total = 0;
+                            @endphp
+                            @if (session('cart'))
+                                @foreach (session('cart') as $id => $details)
+                                    @php
+                                        $total += $details['price'] * $details['quantity'];
+                                        $line_total = $details['price'] * $details['quantity'];
+                                    @endphp
+
+                                    <tr class="align-middle" data-id="{{ $id }}">
+                                        <td data-th="Photo">
+
+                                            <img class="img-responsive"
+                                                src="{{ getPhoto($details['product']['product_image']) }}"
+                                                width="50" />
+
+                                        </td>
+                                        <td data-th="Product">
+                                            <span>{{ $details['product']['product_name'] }}</span>
+                                        </td>
+                                        <td data-th="Price">{{ getPrice($details['price']) }}</td>
+                                        <td data-th="Quantity">
+                                            <input
+                                                class="form-control quantity update-cart allownumericwithoutdecimalMobile"
+                                                type="number" value="{{ $details['quantity'] }}" />
+                                        </td>
+                                        <td class="text-center" data-th="Subtotal">
+                                            {{ getPrice($line_total) }}
+                                        </td>
+                                        <td class="actions" data-th="">
+                                            <button class="btn btn-danger btn-sm remove-from-cart"><i
+                                                    class="fas fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td class="text-center text-danger" colspan="5">
+                                        <h4>No product available in the cart</h4>
+
+                                    </td>
+                                </tr>
+                            @endif
+                        </tbody>
+                        @if (session('cart'))
+                            <tfoot>
+                                <tr>
+                                    <td class="text-end" colspan="4">
+                                        <h5><strong> Total : </strong></h5>
+                                    </td>
+                                    <td class="text-center">{{ getPrice($total) }}</td>
+                                    <td></td>
+                                </tr>
+                            </tfoot>
+                        @endif
+                    </table>
+
+                </div>
+
+                <div class="card shopping_btn fixed-bottom">
+                    <div class="card-body">
+
+                        <div class="d-flex justify-content-between">
+                            <div class="button">
+                                <a class="btn btn-primary"
+                                    href="{{ route('card.preview', $business_card_details->card_url) }}"><i
+                                        class="fa fa-angle-left"></i> Back to
+                                    Shopping</a>
+                            </div>
+
+                            @if (session()->has('cart') && count(session()->get('cart')) > 0)
+                                <div class="button">
+                                    <a class="btn btn-success"
+                                        href="{{ route('checkout', ['cardUrl' => $business_card_details->card_url]) }}">Checkout</a>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
 
         </div>
     </div>
-
 
     @push('script')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="{{ asset('frontend/whatsapp-store/js/script.js') }}"></script>
 
-
         <script>
             $(".allownumericwithoutdecimal").on("keypress keyup blur", function(event) {
                 $(this).val($(this).val().replace(/[^\d].+/, ""));
+                $('.allownumericwithoutdecimalMobile').val($(this).val().replace(/[^\d].+/, ""))
+
+                if ((event.which < 48 || event.which > 57)) {
+                    event.preventDefault();
+                }
+            });
+            $(".allownumericwithoutdecimalMobile").on("keypress keyup blur", function(event) {
+                $(this).val($(this).val().replace(/[^\d].+/, ""));
+                $('.allownumericwithoutdecimal').val($(this).val().replace(/[^\d].+/, ""))
                 if ((event.which < 48 || event.which > 57)) {
                     event.preventDefault();
                 }
@@ -138,6 +251,25 @@
                         quantity: ele.parents("tr").find(".quantity").val()
                     },
                     success: function(response) {
+                        window.location.reload();
+                    }
+                });
+            });
+
+            $(".update-cart-mobile").change(function(e) {
+                e.preventDefault();
+                var ele = $(this);
+
+                $.ajax({
+                    url: '{{ route('update.cart') }}',
+                    method: "patch",
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        id: ele.parents("div").attr("data-id"),
+                        quantity: ele.parents("div").find(".quantityMobile").val()
+                    },
+                    success: function(response) {
+
                         window.location.reload();
                     }
                 });
